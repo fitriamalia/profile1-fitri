9a7aaed3b3025af07e54f7b0b9390895d1a6228b
fitriamalia <fitriamalia200222@gmail.com>
2023-04-15 21:51:55 +0700

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>
+      Home
+    </h1>
+    <p>
+      Home Page
+    </p>
+  </div>
+
+</template>
+
+<style scoped>
+
+div{
+  font-size: 40px;
+  text-align: center;
+  font-weight: bolder;
+}
+
+</style>
